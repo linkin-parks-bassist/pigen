0020cc2aca658fbad8deaf69c824da1dd69e76f9
David Farrell <davidjfarrell96+2@gmail.com>
2026-08-21 12:22:40 +0000

    separate the data type interface

---
+++ include/pigen/data_type.h
@@ -0,0 +1,123 @@
+#ifndef PIGEN_DATA_TYPE_H
+#define PIGEN_DATA_TYPE_H
+
+#include <stddef.h>
+
+#include "pigen/ids.h"
+
+typedef struct pigen_semantic_model pigen_semantic_model;
+
+typedef enum {
+	PIGEN_TYPE_LOGIC,
+	PIGEN_TYPE_BIT,
+	PIGEN_TYPE_INTEGER,
+	PIGEN_TYPE_NAMED
+} pigen_semantic_type_kind;
+
+typedef enum {
+	PIGEN_SIGN_IMPLICIT,
+	PIGEN_SIGN_UNSIGNED,
+	PIGEN_SIGN_SIGNED
+} pigen_signedness;
+
+typedef enum {
+	PIGEN_DATA_TYPE_STATE_INVALID,
+	PIGEN_DATA_TYPE_STATE_TWO,
+	PIGEN_DATA_TYPE_STATE_FOUR
+} pigen_state_domain;
+
+typedef enum {
+	PIGEN_UNARY_POSITIVE,
+	PIGEN_UNARY_NEGATE,
+	PIGEN_UNARY_BITWISE_NOT,
+	PIGEN_UNARY_LOGICAL_NOT,
+	PIGEN_UNARY_REDUCTION_AND,
+	PIGEN_UNARY_REDUCTION_NAND,
+	PIGEN_UNARY_REDUCTION_OR,
+	PIGEN_UNARY_REDUCTION_NOR,
+	PIGEN_UNARY_REDUCTION_XOR,
+	PIGEN_UNARY_REDUCTION_XNOR
+} pigen_unary_operator;
+
+typedef enum {
+	PIGEN_BINARY_ADD,
+	PIGEN_BINARY_SUBTRACT,
+	PIGEN_BINARY_MULTIPLY,
+	PIGEN_BINARY_DIVIDE,
+	PIGEN_BINARY_MODULO,
+	PIGEN_BINARY_POWER,
+	PIGEN_BINARY_SHIFT_LEFT,
+	PIGEN_BINARY_SHIFT_RIGHT,
+	PIGEN_BINARY_ARITH_SHIFT_LEFT,
+	PIGEN_BINARY_ARITH_SHIFT_RIGHT,
+	PIGEN_BINARY_LESS,
+	PIGEN_BINARY_LESS_EQUAL,
+	PIGEN_BINARY_GREATER,
+	PIGEN_BINARY_GREATER_EQUAL,
+	PIGEN_BINARY_EQUAL,
+	PIGEN_BINARY_NOT_EQUAL,
+	PIGEN_BINARY_CASE_EQUAL,
+	PIGEN_BINARY_CASE_NOT_EQUAL,
+	PIGEN_BINARY_WILDCARD_EQUAL,
+	PIGEN_BINARY_WILDCARD_NOT_EQUAL,
+	PIGEN_BINARY_BITWISE_AND,
+	PIGEN_BINARY_BITWISE_XOR,
+	PIGEN_BINARY_BITWISE_XNOR,
+	PIGEN_BINARY_BITWISE_OR,
+	PIGEN_BINARY_LOGICAL_AND,
+	PIGEN_BINARY_LOGICAL_OR
+} pigen_binary_operator;
+
+typedef enum {
+	PIGEN_SEMANTIC_SELECT_RANGE,
+	PIGEN_SEMANTIC_SELECT_INDEXED_UP,
+	PIGEN_SEMANTIC_SELECT_INDEXED_DOWN
+} pigen_select_kind;
+
+typedef struct {
+	pigen_const_expr_id left;
+	pigen_const_expr_id right;
+} pigen_packed_dimension;
+
+typedef struct {
+	pigen_semantic_type_kind kind;
+	pigen_signedness signedness;
+	pigen_symbol_id named_symbol;
+	size_t first_dimension;
+	size_t dimension_count;
+} pigen_semantic_type;
+
+pigen_type_id pigen_type_intern(pigen_semantic_model *model,
+	pigen_semantic_type_kind kind, pigen_signedness signedness,
+	pigen_symbol_id named_symbol, const pigen_packed_dimension *dimensions,
+	size_t dimension_count);
+const pigen_semantic_type *pigen_type_get(const pigen_semantic_model *model,
+	pigen_type_id type);
+const pigen_packed_dimension *pigen_type_dimensions(
+	const pigen_semantic_model *model, pigen_type_id type);
+pigen_type_id pigen_type_packed_element(pigen_semantic_model *model,
+	pigen_type_id type);
+pigen_type_id pigen_type_packed_select(pigen_semantic_model *model,
+	pigen_type_id type, pigen_const_expr_id left,
+	pigen_const_expr_id right, pigen_select_kind kind);
+pigen_const_expr_id pigen_type_packed_width(pigen_semantic_model *model,
+	pigen_type_id type);
+pigen_type_id pigen_type_concatenation(pigen_semantic_model *model,
+	const pigen_type_id *types, size_t count);
+pigen_type_id pigen_semantic_integer_type(pigen_semantic_model *model);
+pigen_type_id pigen_semantic_boolean_result_type(pigen_semantic_model *model);
+int pigen_data_type_is_integral(const pigen_semantic_model *model,
+	pigen_type_id type);
+pigen_state_domain pigen_data_type_state_domain(
+	const pigen_semantic_model *model, pigen_type_id type);
+pigen_type_id pigen_data_type_sized_logic(pigen_semantic_model *model,
+	size_t width, pigen_signedness signedness);
+pigen_type_id pigen_data_type_unary_result(pigen_semantic_model *model,
+	pigen_unary_operator operator, pigen_type_id operand);
+pigen_type_id pigen_data_type_binary_result(pigen_semantic_model *model,
+	pigen_binary_operator operator, pigen_type_id left, pigen_type_id right);
+pigen_type_id pigen_data_type_conditional_result(pigen_semantic_model *model,
+	pigen_type_id condition, pigen_type_id when_true,
+	pigen_type_id when_false);
+
+#endif
--- include/pigen/semantic.h
+++ include/pigen/semantic.h
@@ -4,27 +4,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "pigen/data_type.h"
 #include "pigen/source.h"
 #include "pigen/transfer_type.h"
-
-typedef enum {
-	PIGEN_TYPE_LOGIC,
-	PIGEN_TYPE_BIT,
-	PIGEN_TYPE_INTEGER,
-	PIGEN_TYPE_NAMED
-} pigen_semantic_type_kind;
-
-typedef enum {
-	PIGEN_SIGN_IMPLICIT,
-	PIGEN_SIGN_UNSIGNED,
-	PIGEN_SIGN_SIGNED
-} pigen_signedness;
-
-typedef enum {
-	PIGEN_DATA_TYPE_STATE_INVALID,
-	PIGEN_DATA_TYPE_STATE_TWO,
-	PIGEN_DATA_TYPE_STATE_FOUR
-} pigen_state_domain;
 
 typedef enum {
 	PIGEN_SYMBOL_SIGNAL,
@@ -74,54 +56,6 @@ enum {
 };
 
 typedef enum {
-	PIGEN_UNARY_POSITIVE,
-	PIGEN_UNARY_NEGATE,
-	PIGEN_UNARY_BITWISE_NOT,
-	PIGEN_UNARY_LOGICAL_NOT,
-	PIGEN_UNARY_REDUCTION_AND,
-	PIGEN_UNARY_REDUCTION_NAND,
-	PIGEN_UNARY_REDUCTION_OR,
-	PIGEN_UNARY_REDUCTION_NOR,
-	PIGEN_UNARY_REDUCTION_XOR,
-	PIGEN_UNARY_REDUCTION_XNOR
-} pigen_unary_operator;
-
-typedef enum {
-	PIGEN_BINARY_ADD,
-	PIGEN_BINARY_SUBTRACT,
-	PIGEN_BINARY_MULTIPLY,
-	PIGEN_BINARY_DIVIDE,
-	PIGEN_BINARY_MODULO,
-	PIGEN_BINARY_POWER,
-	PIGEN_BINARY_SHIFT_LEFT,
-	PIGEN_BINARY_SHIFT_RIGHT,
-	PIGEN_BINARY_ARITH_SHIFT_LEFT,
-	PIGEN_BINARY_ARITH_SHIFT_RIGHT,
-	PIGEN_BINARY_LESS,
-	PIGEN_BINARY_LESS_EQUAL,
-	PIGEN_BINARY_GREATER,
-	PIGEN_BINARY_GREATER_EQUAL,
-	PIGEN_BINARY_EQUAL,
-	PIGEN_BINARY_NOT_EQUAL,
-	PIGEN_BINARY_CASE_EQUAL,
-	PIGEN_BINARY_CASE_NOT_EQUAL,
-	PIGEN_BINARY_WILDCARD_EQUAL,
-	PIGEN_BINARY_WILDCARD_NOT_EQUAL,
-	PIGEN_BINARY_BITWISE_AND,
-	PIGEN_BINARY_BITWISE_XOR,
-	PIGEN_BINARY_BITWISE_XNOR,
-	PIGEN_BINARY_BITWISE_OR,
-	PIGEN_BINARY_LOGICAL_AND,
-	PIGEN_BINARY_LOGICAL_OR
-} pigen_binary_operator;
-
-typedef enum {
-	PIGEN_SEMANTIC_SELECT_RANGE,
-	PIGEN_SEMANTIC_SELECT_INDEXED_UP,
-	PIGEN_SEMANTIC_SELECT_INDEXED_DOWN
-} pigen_select_kind;
-
-typedef enum {
 	PIGEN_SEMANTIC_INTERNAL,
 	PIGEN_SEMANTIC_INPUT,
 	PIGEN_SEMANTIC_OUTPUT,
@@ -132,11 +66,6 @@ typedef enum {
 	PIGEN_SEMANTIC_POSEDGE,
 	PIGEN_SEMANTIC_NEGEDGE
 } pigen_semantic_edge;
-
-typedef struct {
-	pigen_const_expr_id left;
-	pigen_const_expr_id right;
-} pigen_packed_dimension;
 
 typedef enum {
 	PIGEN_SHAPE_DIMENSION_COUNT,
@@ -158,14 +87,6 @@ typedef struct {
 	size_t first_dimension;
 	size_t dimension_count;
 } pigen_semantic_shape;
-
-typedef struct {
-	pigen_semantic_type_kind kind;
-	pigen_signedness signedness;
-	pigen_symbol_id named_symbol;
-	size_t first_dimension;
-	size_t dimension_count;
-} pigen_semantic_type;
 
 typedef struct {
 	pigen_scope_id parent;
@@ -378,7 +299,7 @@ typedef struct {
 	unsigned roles;
 } pigen_transfer_signal_use;
 
-typedef struct {
+struct pigen_semantic_model {
 	const pigen_source_manager *sources;
 	pigen_semantic_type *types;
 	size_t type_count;
@@ -452,7 +373,7 @@ typedef struct {
 	pigen_shape_id scalar_shape;
 	pigen_predicate_id true_predicate;
 	pigen_predicate_id false_predicate;
-} pigen_semantic_model;
+};
 
 typedef enum {
 	PIGEN_DECLARE_INVALID,
@@ -462,14 +383,6 @@ typedef enum {
 
 void pigen_semantic_init(pigen_semantic_model *model,
 	const pigen_source_manager *sources);
-pigen_type_id pigen_type_intern(pigen_semantic_model *model,
-	pigen_semantic_type_kind kind, pigen_signedness signedness,
-	pigen_symbol_id named_symbol, const pigen_packed_dimension *dimensions,
-	size_t dimension_count);
-const pigen_semantic_type *pigen_type_get(const pigen_semantic_model *model,
-	pigen_type_id type);
-const pigen_packed_dimension *pigen_type_dimensions(
-	const pigen_semantic_model *model, pigen_type_id type);
 pigen_shape_id pigen_shape_intern(pigen_semantic_model *model,
 	const pigen_shape_dimension *dimensions, size_t dimension_count);
 pigen_shape_id pigen_semantic_scalar_shape(pigen_semantic_model *model);
@@ -479,30 +392,6 @@ const pigen_shape_dimension *pigen_shape_dimensions(
 	const pigen_semantic_model *model, pigen_shape_id shape);
 pigen_shape_id pigen_shape_element(pigen_semantic_model *model,
 	pigen_shape_id shape);
-pigen_type_id pigen_type_packed_element(pigen_semantic_model *model,
-	pigen_type_id type);
-pigen_type_id pigen_type_packed_select(pigen_semantic_model *model,
-	pigen_type_id type, pigen_const_expr_id left,
-	pigen_const_expr_id right, pigen_select_kind kind);
-pigen_const_expr_id pigen_type_packed_width(pigen_semantic_model *model,
-	pigen_type_id type);
-pigen_type_id pigen_type_concatenation(pigen_semantic_model *model,
-	const pigen_type_id *types, size_t count);
-pigen_type_id pigen_semantic_integer_type(pigen_semantic_model *model);
-pigen_type_id pigen_semantic_boolean_result_type(pigen_semantic_model *model);
-int pigen_data_type_is_integral(const pigen_semantic_model *model,
-	pigen_type_id type);
-pigen_state_domain pigen_data_type_state_domain(
-	const pigen_semantic_model *model, pigen_type_id type);
-pigen_type_id pigen_data_type_sized_logic(pigen_semantic_model *model,
-	size_t width, pigen_signedness signedness);
-pigen_type_id pigen_data_type_unary_result(pigen_semantic_model *model,
-	pigen_unary_operator operator, pigen_type_id operand);
-pigen_type_id pigen_data_type_binary_result(pigen_semantic_model *model,
-	pigen_binary_operator operator, pigen_type_id left, pigen_type_id right);
-pigen_type_id pigen_data_type_conditional_result(pigen_semantic_model *model,
-	pigen_type_id condition, pigen_type_id when_true,
-	pigen_type_id when_false);
 pigen_const_expr_id pigen_const_expr_intern_integer(
 	pigen_semantic_model *model, uint64_t value, pigen_type_id type);
 pigen_const_expr_id pigen_const_expr_intern_bits(pigen_semantic_model *model,
